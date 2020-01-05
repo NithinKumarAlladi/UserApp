2f57a47fb38003525c1a8b78959360fec85a57fe
NithinKumarAlladi <nithinkumaralladi.me@gmail.com>
2020-01-05 20:16:41 +0530

    Added the POST request

--- user.js
+++ user.js
@@ -1,5 +1,8 @@
 function getData(){
+    const name = document.getElementById(nameTag).innerText;
+    console.log(name);
     var url = "http://localhost:3001/show";
+    Request()
     fetch(url,{method:"GET"}).then((res)=>{
         res.json().then((data)=>{
             console.log(data);
@@ -9,12 +12,21 @@ function getData(){
 }
 function submit(){
     var data = {
-        firstName:'',
-        lastName:'',
-        address:''
-    }
-    data.firstName = document.getElementById('fname').innerText;
-    data.lastName = document.getElementById('lname').innerText;
-    data.address = document.getElementById('address').innerText;
+        firstName:document.getElementById('fname').innerText,
+        lastName:document.getElementById('lname').innerText,
+        address:document.getElementById('address').innerText
+    };
+    var url = 'http://localhost:3001/data'
+    // data.firstName = document.getElementById('fname').innerText;
+    // data.lastName = document.getElementById('lname').innerText;
+    // data.address = document.getElementById('address').innerText;
+    fetch(url,{
+        method:'POST',
+        headers:{
+            'Accept':'application/json, text/plain, */* ',
+            'Content-type': "application/json"
+        },
+        body: JSON.stringify(data),
+    }).then((res)=> res.json()).then((userPost)=> console.log(userPost));
     
 }
